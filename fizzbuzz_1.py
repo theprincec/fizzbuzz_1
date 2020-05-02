031a6d2c435495c7a8739258ce2a80b8376eee83
Christian Cumberlander <christiancumberlander@ChristiansMBP4.attlocal.net>
2020-05-02 01:09:06 -0400

    Added 'else' and for loop

--- fizzbuzz_1.py
+++ fizzbuzz_1.py
@@ -1,9 +1,12 @@
-def fizzbuzz(num):
-    if num % 3 == 0 and num % 5 == 0:
-        print('Fizzbuzz')
-    elif num % 3 == 0:
-        print('Fizz')
-    elif num % 5 == 0:
-        print('Buzz')
-
-fizzbuzz(15)
+# a=[3,7,9,10,12,15,16,20]
+def fizzbuzz(listy):
+    for num in listy:
+        if num % 3 == 0 and num % 5 == 0:
+            print('Fizzbuzz')
+        elif num % 3 == 0:
+            print('Fizz')
+        elif num % 5 == 0:
+            print('Buzz')
+        else:
+            print(num)
+# fizzbuzz(a)
